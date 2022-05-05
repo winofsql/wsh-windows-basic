4fb01122d00df3c880295f931abca1016e85bdb2
sworc <lightbox@nifty.com>
2022-05-05 19:08:29 +0900

    Add files via upload

--- bat/php-file-download.bat
+++ bat/php-file-download.bat
@@ -1,1 +1,3 @@
 php -r "file_put_contents('c:\tmp\download.jpg',file_get_contents('https://winofsql.jp/image/daz/2021-08-03-1.jpg'));"
+
+php -r "copy('https://winofsql.jp/image/daz/2021-08-03-1.jpg','c:\tmp\download2.jpg');"
